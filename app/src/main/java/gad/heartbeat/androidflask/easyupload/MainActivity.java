766dd2ff1576dfaf86ede2e3c963558100334a0a
simran029 <simranbajaj029@gmail.com>
2020-06-02 16:37:18 +0530

    Second Commit

--- app/src/main/java/gad/heartbeat/androidflask/easyupload/MainActivity.java
+++ app/src/main/java/gad/heartbeat/androidflask/easyupload/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.ClipData;
 import android.content.ContentUris;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -12,10 +13,12 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.speech.tts.TextToSpeech;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,7 +29,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.List;
 
 //import dmax.dialog.SpotsDialog;
@@ -38,6 +43,7 @@ import android.speech.tts.TextToSpeech;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -72,9 +78,11 @@ public class MainActivity extends AppCompatActivity {
     public TextToSpeech mTTS;
     public EditText mEditText;
     public Button mButtonSpeak;
-    CameraKitView cameraView;
+    ImageView cameraView;
     Button btn_detect;
 	int i=1;
+	boolean imagesSelected = false;
+	ArrayList<String> selectedImagesPaths;
     AlertDialog waitingDialog;
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
@@ -86,64 +94,79 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        cameraView.onStart();
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        cameraView.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        cameraView.onPause();
-        super.onPause();
-    }
-
-
-    final int SELECT_MULTIPLE_IMAGES = 1;
-    ArrayList<String> selectedImagesPaths; // Paths of the image(s) selected by the user.
-    boolean imagesSelected = false; // Whether the user selected at least an image or not.
-
+	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.INTERNET}, 2);
-        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
         setContentView(R.layout.activity_main);
-		connectServer();
-        cameraView = (CameraKitView)findViewById(R.id.camera_view);
+        cameraView = (ImageView)findViewById(R.id.camera_view);
         btn_detect = (Button)findViewById(R.id.btn_detect);
         btn_detect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                cameraView.captureImage(new CameraKitView.ImageCallback() {
-                    @Override
-                    public void onImage(CameraKitView cameraKitView, final byte[] capturedImage) {
-                        String basepath = Environment.getExternalStorageDirectory().getAbsolutePath();
-                        TextView ext = findViewById(R.id.status);
-                        ext.setText(basepath);
-						String filename = "photo"+i+".jpg";
-						File savedPhoto = new File(basepath, filename);
-                        try {
-                            FileOutputStream outputStream = new FileOutputStream(savedPhoto.getPath());
-                            outputStream.write(capturedImage);
-							i = i+1;
-                            outputStream.close();
-                        } catch (java.io.IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
+                StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+                StrictMode.setVmPolicy(builder.build());
+                selectImage();
             }
         });
+		mButtonSpeak = findViewById(R.id.button_speak);
+        mEditText = findViewById(R.id.status);
+        mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = mTTS.setLanguage(Locale.ENGLISH);
+                    if (result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "Language not supported");
+                    } else {
+                        mButtonSpeak.setEnabled(true);
+                    }
+                } else {
+                    Log.e("TTS", "Initialization failed");
+                }
+            }});
+
+        mButtonSpeak.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        speak();
+                    }
+                });
 
     }
-
+ private void selectImage() {
+        final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Add Photo!");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Take Photo"))
+                {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File f = new File(Environment.getExternalStorageDirectory().toString(), "temp.jpg");
+                    Uri imageUri = FileProvider.getUriForFile(
+                            MainActivity.this,
+                            getApplicationContext().getPackageName()+".provider", //(use your app signature + ".provider" )
+                            f);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                    startActivityForResult(intent, 1);
+                }
+                else if (options[item].equals("Choose from Gallery"))
+                {
+                    Intent intent = new   Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, 2);
+                }
+                else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
@@ -167,28 +190,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void connectServer() {
-        TextView responseText = findViewById(R.id.status);
-        responseText.setText("Sending the Files. Please Wait ...");
+        //mEditText.setText(selectedImagesPaths);
         String ipv4Address = "192.168.0.106";
         String portNumber = "5000";
         String postUrl = "http://" + ipv4Address + ":" + portNumber + "/";
 
+		
         MultipartBody.Builder multipartBodyBuilder = new MultipartBody.Builder().setType(MultipartBody.FORM);
+		
+            for (int i = 0; i < selectedImagesPaths.size(); i++) {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.RGB_565;
+           // options.inSampleSize= 8;
+            //options.inJustDecodeBounds= false;
+           // File f= new File(selectedImagesPaths.get(i));
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            try {
+                // Read BitMap by file path.
+               // mEditText.setText("line1");
+                Bitmap bitmap = BitmapFactory.decodeFile(selectedImagesPaths.get(i), options);
+                if(bitmap==null)
+                {
+                   // mEditText.setText("null");
+                }
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+            }catch(Exception e){
+              //  mEditText.setText("hi"+ selectedImagesPaths.get(i)+e.getMessage());
+                return;
+            }
+            byte[] byteArray = stream.toByteArray();
+
+            multipartBodyBuilder.addFormDataPart("image" + i, "Android_Flask_" + i + ".jpg", RequestBody.create(MediaType.parse("image/*jpg"), byteArray));
+        }
+
+        RequestBody postBodyImage = multipartBodyBuilder.build();
 
 
-
-        //postRequest(postUrl, postBodyImage);
+        postRequest(postUrl, postBodyImage);
     }
 
     void postRequest(String postUrl, RequestBody postBody) {
 
-        OkHttpClient client = new OkHttpClient();
+        OkHttpClient client = new OkHttpClient.Builder().readTimeout(100, TimeUnit.SECONDS).build();
 
         Request request = new Request.Builder()
                 .url(postUrl)
                 .post(postBody)
                 .build();
 
+        Log.w("reqyest","ho ja ");
+        //mEditText.setText(postBody.toString());
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
@@ -214,7 +265,7 @@ public class MainActivity extends AppCompatActivity {
                     public void run() {
                         TextView responseText = findViewById(R.id.status);
                         try {
-                            responseText.setText("Server's Response\n" + response.body().string());
+                            responseText.setText(response.body().string());
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
@@ -224,35 +275,110 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void selectImage(View v) {
-        Intent intent = new Intent();
-        intent.setType("*/*");
-        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_MULTIPLE_IMAGES);
-    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK)
-        {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            ImageView img = findViewById(R.id.img);
-            img.setImageBitmap(imageBitmap);
-
-        }
-            try {
-
-
-
-        } catch (Exception e) {
-            Toast.makeText(this, "Something Went Wrong.", Toast.LENGTH_LONG).show();
-            e.printStackTrace();
-        }
-
         super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                File f = new File(Environment.getExternalStorageDirectory(), "temp.jpg");
+                if(data != null){
+                for (File temp : f.listFiles()) {
+                    if (temp.getName().equals("temp.jpg")) {
+                        f = temp;
+                        break;
+                    }
+                }}
+                try {
+                    Bitmap bitmap;
+                    Log.w("chl","1");
+                    BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+                    bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(),
+                            bitmapOptions);
+                    cameraView.setImageBitmap(bitmap);
+                    Log.w("chl","2");
+//                    String path = android.os.Environment.getExternalStorageDirectory()+ File.separator+ "know_it_all";
+//                    String path = File.separator + "storage" + File.separator + "emulated" + File.separator + "0" +File.separator+ "DCIM"+File.separator+"Camera"+File.separator;
+                    String path = File.separator + "storage/emulated/0/DCIM/Camera/";
+//                    f.delete();
+                    Log.w("chl","3");
+                    OutputStream outFile = null;
+                    String system_time = String.valueOf(System.currentTimeMillis());
+                    File file = new File(path, system_time + ".jpg");
+                    Log.w("chl","4");
+                    try {
+                        outFile = new FileOutputStream(file);
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 85, outFile);
+                        Log.w("kuch ho ja", "something is happening");
+                        mEditText.setText("jai ho");
+                       selectedImagesPaths = new ArrayList<>();
+                        String currPath = path + system_time + ".jpg";
+                        mEditText.setText(currPath);
+
+                        //selectedImagesPaths.add(currPath);
+                       // connectServer();
+//                        cameraView.setImageBitmap(thumbnail);
+                        outFile.flush();
+                        outFile.close();
+                        selectedImagesPaths.add(currPath);
+                        connectServer();
+				/*Uri selectedImage = data.getData();
+				String currImagesPaths = getPath(getApplicationContext(), selectedImage);
+				Log.d("ImageDetails", "Single Image URI : " + selectedImage);
+                Log.d("ImageDetails", "Single Image Path : " + currImagesPaths);*/
+               // mEditText.setText(currPath);
+				//selectedImagesPaths.add(currPath);
+                       // connectServer();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (requestCode == 2) {
+				if(data.getData()!=null)
+				{
+				selectedImagesPaths = new ArrayList<>();
+				Uri selectedImage = data.getData();
+				String currImagesPaths = getPath(getApplicationContext(), selectedImage);
+				Log.d("ImageDetails", "Single Image URI : " + selectedImage);
+                Log.d("ImageDetails", "Single Image Path : " + currImagesPaths);
+				mEditText.setText(currImagesPaths);
+                selectedImagesPaths.add(currImagesPaths);
+				
+                
+                String[] filePath = { MediaStore.Images.Media.DATA };
+                Cursor c = getContentResolver().query(selectedImage,filePath, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                String picturePath = c.getString(columnIndex);
+                c.close();
+                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                Log.w("path of image", picturePath+"");
+               // mEditText.setText(selectedImagesPaths);
+                cameraView.setImageBitmap(thumbnail);
+                connectServer();
+				}
+            }
+        }
     }
+	
+	public void speak() {
+            String text = mEditText.getText().toString();
+            mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+        @Override
+        public void onDestroy() {
+            if (mTTS != null) {
+                mTTS.stop();
+                mTTS.shutdown();
+            }
+            super.onDestroy();
+        }
 
     // Implementation of the getPath() method and all its requirements is taken from the StackOverflow Paul Burke's answer: https://stackoverflow.com/a/20559175/5426539
     public static String getPath(final Context context, final Uri uri) {
